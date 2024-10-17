afbf1b5a16f2e3ae5d9483c5b061db82239a0b3f
AkashKank <akashkank4@gmail.com>
2024-10-17 11:53:28 +0530

    reverse number triangle pattern

--- reversenumberpatt.py
+++ reversenumberpatt.py
@@ -4,9 +4,14 @@ def main():
 
     number2 = int(input("Enter Number for printimg reverse number pattern : "))
 
-    for i in range(number2, 0, -1):
-        for j in range(i):
-            print(i, end="")
+    # for i in range(number2, 0, -1):
+    #     for j in range(i):
+    #         print(i, end="")
+    #     print()
+
+    for i in range(1, number2+1):
+        for j in range(i, 0, -1):
+            print(j, end="")
         print()
 
 
